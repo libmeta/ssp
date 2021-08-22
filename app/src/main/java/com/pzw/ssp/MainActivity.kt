400cbb521453199086e607cf1187b02f2f25b799
pengzhiwen <maurice.svip@gmail.com>
2021-08-22 12:00:48 +0800

    first commit

---
+++ app/src/main/java/com/pzw/ssp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.pzw.ssp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.pzw.ssp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+    /**
+      * A native method that is implemented by the 'ssp' native library,
+      * which is packaged with this application.
+      */
+     external fun stringFromJNI(): String
+
+     companion object {
+         // Used to load the 'ssp' library on application startup.
+         init {
+             System.loadLibrary("ssp")
+         }
+     }
+}
